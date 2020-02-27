def55fc1f3ec912df021da68e28f9156b94e3ffb
Cameron Reikes <cameronreikes@gmail.com>
2020-02-26 18:45:05 -0800

    Puddle spawning

--- src/PuddleSpawner.gd
+++ src/PuddleSpawner.gd
@@ -1,15 +1,11 @@
 extends Node2D
-
-var initial_player_pos: Vector2 = Vector2()
-
 var last_puddle = null
 
 func _ready():
 	randomize()
-	initial_player_pos = get_node("../Player").global_position
 	
-	var cur_spawn_position: Vector2 = initial_player_pos
-	while cur_spawn_position.x < 2500.0:
+	var cur_spawn_position: Vector2 = get_node("InitialPosition").global_position
+	while cur_spawn_position.x < 5000.0:
 		last_puddle = spawn_puddle(cur_spawn_position)
 		cur_spawn_position = last_puddle.global_position
 
@@ -22,4 +18,5 @@ func spawn_puddle(initial_position: Vector2) -> Node2D:
 	var cur_puddle: Node2D = preload("res://Puddle.tscn").instance()
 	add_child(cur_puddle)
 	cur_puddle.global_position = new_position
+	cur_puddle.connect("despawning", self, "on_puddle_despawned")
 	return cur_puddle
